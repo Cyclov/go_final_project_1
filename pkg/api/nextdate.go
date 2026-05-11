891627464210ce2f38ff520f3c6164261b8d392e
Andrey Suslov <cyclov@list.ru>
2026-05-12 01:18:28 +0300

    fix

--- pkg/api/nextdate.go
+++ pkg/api/nextdate.go
@@ -212,5 +212,10 @@ func nextDayHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, result)
+	_, err = fmt.Fprint(w, result)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
